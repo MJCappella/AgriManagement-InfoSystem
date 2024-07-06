2d9492d80cecc48652f60b70b3c6a9fa77b60c6c
Peter Ndungu <peter.ndungu@softclans.co.ke>
2024-07-06 21:14:20 +0300

    Added icons

--- includes/header.php
+++ includes/header.php
@@ -6,14 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>/assets/css/styles.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/assets/css/fontawesome/css/all.min.css">
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>/assets/css/bootstrap.min.css" integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="<?php echo BASE_URL; ?>/assets/js/scripts.js" defer></script>
     <script src="<?php echo BASE_URL; ?>/assets/js/jquery-3.7.1.min.js" defer></script>
     <script src="<?php echo BASE_URL; ?>/assets/js/popper.min.js" defer></script>
     <script src="<?php echo BASE_URL; ?>/assets/js/d3.min.js" defer></script>
     <script src="<?php echo BASE_URL; ?>/assets/js/bootstrap.min.js" integrity="sha512-ykZ1QQr0Jy/4ZkvKuqWn4iF3lqPZyij9iRv6sGqLRdTPkY69YX6+7wvVGmsdBbiIfN/8OdsI7HABjvEok6ZopQ==" crossorigin="anonymous" referrerpolicy="no-referrer" defer></script>
-
 </head>
 
 <body>
     <div class="container">
+        
--- pages/home.php
+++ pages/home.php
@@ -4,8 +4,9 @@ include_once('../includes/header.php');
 ?>
 
 <h1>Welcome to the Agriculture Marketing Information System</h1>
-<button class="btn btn-info" onclick="showWelcomeMessage()">Click Me</button>
-
-
+<button class="btn btn-info" onclick="showWelcomeMessage()"><i class="fas fa-user"></i>  Click Me</button>
+<div class="alert alert-info" role="alert">
+ Welcome to the Agriculture Marketing Information System
+</div>
 
 <?php include_once('../includes/footer.php'); ?>
